090d30cab4867857882b41688d00eef8df6fa885
Kristin Barr <kristinbarr@gmail.com>
2020-01-17 00:14:00 -0500

    MVP for day 3

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,19 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
-
 
 # Link rooms together
 
@@ -49,3 +42,33 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# welcome message
+player = Player("Player1", room["outside"])
+print(f"\nWelcome to the Mad House, {player.name}!")
+print(f"\nYou are here: {player.cur_room}")
+print("\nGo [n] North [s] South [e] East [w] West  or  [q] to Quit.")
+# print("Hit [n] to enter the mad house...\n")
+# print(player.cur_room.name)
+
+# initialize cmd
+directions = ["n", "s", "e", "w"]
+
+# LOOP
+while True:
+    # READ
+    # print(f'welcome {player.name}')
+    cmd = input("~~> ").lower()
+
+    # PRINT
+    # current room name description
+    if cmd in directions:
+        player.travel(cmd)
+    # if user quits game
+    elif cmd == "q":
+        print("\nGoodbye!\n")
+        exit()
+    else:
+        # prompt user to make another selection
+        print('\n No room here. Please choose another direction!\n')
+        # print("[n] North [s] South [e] East [w] West   [q] Quit")
--- src/player.py
+++ src/player.py
@@ -1,2 +1,29 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
+# Write a class to hold player information, e.g. what room they are in currently.
+class Player:
+    def __init__(self, name, start_room):
+        self.name = name
+        self.cur_room = start_room
+        # self.item = item
+        # self.holding = holding
+
+    def travel(self, direction):
+        # player should be able to travel in a direction
+        # below creates a variable and calls a method on a room instance
+        next_room = self.cur_room.get_next_room(direction)
+        if next_room is not None:
+            self.cur_room = next_room
+            print(f"You are in the: {self.cur_room}")
+        else:
+            print("No room in that direction, try again!")
+
+    # def travel(self, direction):
+    #     next_room = self.cur_room.get_room_in_direction(direction)
+    #     if next_room is not None:
+    #         self.cur_room = next_room
+    #     else:
+    #         print("You cannot move in that direction.")
+
+    # def grab(self):
+    #     holding.append(self.item)
+    # def drop(self):
+    #     holding.remove(self.item)
