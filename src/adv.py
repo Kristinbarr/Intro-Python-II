393a3294a8bb4bcd322a2b8545c4ece473185d82
Kristin Barr <kristinbarr@gmail.com>
2020-02-13 14:30:08 -0500

    basic working solution

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
+import sys
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -21,9 +23,7 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -35,12 +35,10 @@ room['treasure'].s_to = room['narrow']
 
 #
 # Main
-#
 
 # Make a new player object that is currently in the 'outside' room.
 
 # Write a loop that:
-#
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
@@ -49,3 +47,32 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+    
+player1 = Player('Joe', room['outside'], 'n')
+print()
+print("======= Welcome to the Dungeon =======")
+print()
+# playerName = input('What is your name? ')
+# print(f'welcome {playerName}')
+# print(player1.printRoom())
+cmd = input("Press [n][s][e][w] to move through rooms, [q] to quit.")
+
+while True:
+    print(player1.cur_room)
+    cmd = input('--> ').lower()
+
+    if cmd in ['n','s','e','w']:
+        current_room = player1.cur_room
+        next_room = getattr(current_room, f"{cmd}_to")
+
+        if next_room is not None:
+            # move player by setting new cur room
+            player1.cur_room = player1.cur_room.n_to
+        else:
+            print("You cannot move in that direction.")
+
+    elif cmd == 'q':
+        print('Goodbye!')
+        exit()
+    else:
+        print('Not a valid command, try again! [n][s][e][w] or [q]')
--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, cur_room, direction):
+        self.name = name
+        self.cur_room = cur_room
+        self.direction = direction
+
+    def printRoom(self):
+        return f'You are currently located: {self.cur_room}'
+
+    def move(self, new_room):
+        self.cur_room = new_room
